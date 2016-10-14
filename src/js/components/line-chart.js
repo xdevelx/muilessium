61e6c51c35b3d7aff13bc7338d728c37d8b9ccb9
Ivan Bogachev <sfi0zy@gmail.com>
2016-10-14 12:19:44 +0300

    Add filled line chart

--- src/js/components/line-chart.js
+++ src/js/components/line-chart.js
@@ -4,19 +4,21 @@ import { Component } from '../component';
 let template = {
     open: '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><line stroke-width="1" stroke="#777" x1="0" x2="100" y1="0" y2="0"></line><line stroke-width="1" stroke="#777" x1="0" x2="0" y1="0" y2="100"></line>',
     line: '<polyline fill="none" stroke-width="1" stroke="{{color}}" points="{{points}}"></polyline>',
+    filledLine: '<polygon fill="{{color}}" fill-opacity=".5" stroke="none" points="{{points}}"></polygon>',
     close: '</svg>'
 };
 
 class Line {
-    constructor(data) {
+    constructor(data, isFilled) {
         data = data.trim();
 
         this.color = data.match(/(#[0-9A-F]+)/i)[1];
         this.values = data.replace(this.color, '').trim().split(' ');
+        this.isFilled = isFilled;
     }
 
     render(maxValue) {
-        let step = 100 / this.values.length,
+        let step = 100 / (this.values.length - 1),
             points = '';
 
         this.values.forEach(function(value, i) {
@@ -24,6 +26,15 @@ class Line {
 
             points += currentPoint;
         });
+
+        if (this.isFilled) {
+            points += ((this.values.length - 1) * step + ',0 ');
+            points += '0,0 ';
+
+            return template.filledLine
+                .replace('{{color}}',  this.color)
+                .replace('{{points}}', points);
+        }
 
         return template.line
             .replace('{{color}}',  this.color)
@@ -43,8 +54,10 @@ export class LineChart extends Component {
         this.lines = [];
         this.maxValue = 0;
 
+        let isFilled = (element.className.indexOf('-filled') !== -1);
+
         for (let i = 0; i < this.data.length; i++) {
-            let line = new Line(this.data[i]);
+            let line = new Line(this.data[i], isFilled);
 
             for (let j = 0; j < line.values.length; j++) {
                 if (+line.values[j] > this.maxValue) {
