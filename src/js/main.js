c3ff57c96d9340844f039cf7f7690c33ff8f1258
Ivan Bogachev <sfi0zy@gmail.com>
2016-12-11 19:09:55 +0300

    Create test branch with a number of components

--- src/js/main.js
+++ src/js/main.js
@@ -9,20 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
     window.Muilessium = new Muilessium;
 
     window.Muilessium.components = {
-        inputs:       window.Muilessium.create('input',             '.mui-input',             {}),
-        textareas:    window.Muilessium.create('textarea',          '.mui-textarea',          {}),
-        likes:        window.Muilessium.create('like',              '.mui-like',              {}),
-        buttons:      window.Muilessium.create('button',            '.mui-button',            {}),
-        buttonsd:     window.Muilessium.create('button-dropdown',   '.mui-button-dropdown',   {}),
-        carousels:    window.Muilessium.create('carousel',          '.mui-carousel',          {}),
-        piecharts:    window.Muilessium.create('pie-chart',         '.mui-pie-chart',         {}),
-        barcharts:    window.Muilessium.create('bar-chart',         '.mui-bar-chart',         {}),
-        linecharts:   window.Muilessium.create('line-chart',        '.mui-line-chart',        {}),
-        dropdowns:    window.Muilessium.create('select-dropdown',   '.mui-select-dropdown',   {}),
         accordions:   window.Muilessium.create('accordion',         '.mui-accordion',         {}),
+        buttonsd:     window.Muilessium.create('button-dropdown',   '.mui-button-dropdown',   {}),
+        buttons:      window.Muilessium.create('button',            '.mui-button',            {}),
+        carousels:    window.Muilessium.create('carousel',          '.mui-carousel',          {}),
         checkboxes:   window.Muilessium.create('checkbox',          '.mui-checkbox',          {}),
-        tabs:         window.Muilessium.create('tabs',              '.mui-tabs',              {}),
         navigations:  window.Muilessium.create('header-navigation', '.mui-header-navigation', {}),
-        progressbars: window.Muilessium.create('progress-bar',      '.mui-progress-bar',      {})
+        inputs:       window.Muilessium.create('input',             '.mui-input',             {}),
+        dropdowns:    window.Muilessium.create('select-dropdown',   '.mui-select-dropdown',   {}),
+        tabs:         window.Muilessium.create('tabs',              '.mui-tabs',              {}),
+        textareas:    window.Muilessium.create('textarea',          '.mui-textarea',          {})
     };
 });
--- src/js/muilessium.js
+++ src/js/muilessium.js
@@ -1,17 +1,12 @@
 import * as Utils from './utils';
 
 import { Accordion        } from './components/accordion';
-import { BarChart         } from './components/bar-chart';
-import { Button           } from './components/button';
 import { ButtonDropdown   } from './components/button-dropdown';
+import { Button           } from './components/button';
 import { Carousel         } from './components/carousel';
 import { Checkbox         } from './components/checkbox';
 import { HeaderNavigation } from './components/header-navigation';
 import { Input            } from './components/input';
-import { Like             } from './components/like';
-import { LineChart        } from './components/line-chart';
-import { PieChart         } from './components/pie-chart';
-import { ProgressBar      } from './components/progress-bar';
 import { SelectDropdown   } from './components/select-dropdown';
 import { Tabs             } from './components/tabs';
 import { Textarea         } from './components/textarea';
@@ -19,17 +14,12 @@ import { Textarea         } from './components/textarea';
 
 const components = {
     'accordion':         Accordion,
-    'bar-chart':         BarChart,
-    'button':            Button,
     'button-dropdown':   ButtonDropdown,
+    'button':            Button,
     'carousel':          Carousel,
     'checkbox':          Checkbox,
     'header-navigation': HeaderNavigation,
     'input':             Input,
-    'like':              Like,
-    'line-chart':        LineChart,
-    'pie-chart':         PieChart,
-    'progress-bar':      ProgressBar,
     'select-dropdown':   SelectDropdown,
     'tabs':              Tabs,
     'textarea':          Textarea
