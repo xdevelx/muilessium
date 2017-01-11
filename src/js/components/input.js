5d9b0977986264fdda1ac427081677927b8e4b7b
Ivan Bogachev <sfi0zy@gmail.com>
2017-01-11 10:58:36 +0300

    Add inputs validation

--- src/js/components/input.js
+++ src/js/components/input.js
@@ -9,6 +9,14 @@ export class Input extends Component {
         this.dom = Utils.extend(this.dom, {
             input: element.getElementsByTagName('input')[0],
             labels: element.parentNode.getElementsByTagName('label'),
+            hint: element.parentNode.getElementsByClassName('mui-input-hint')[0],
+            indicator: element.parentNode.getElementsByClassName('mui-input-indicator')[0]
+        });
+
+        this.state = Utils.extend(this.state, {
+            regexp: new RegExp((element.getAttribute('data-regexp') || '')),
+            validationDelay: (element.getAttribute('data-validation-delay') || 300),
+            validationTimeout: null
         });
 
         this.initAria();
@@ -40,30 +48,89 @@ export class Input extends Component {
             });
         });
 
-        this.dom.input.addEventListener('focus', () => {
-            Utils.addClass(this.element, '-focused');
+        this.dom.input.addEventListener('focus', this.focusHandler.bind(this));
+        this.dom.input.addEventListener('blur',  this.blurHandler.bind(this));
 
-            Utils.ifNodeList(this.dom.labels, () => {
-                Utils.makeElementsNotFocusable(this.dom.labels);
-            });
-        });
+        this.dom.input.addEventListener('change',  this.changeValueHandler.bind(this));
+        this.dom.input.addEventListener('keydown', this.changeValueHandler.bind(this));
 
-        this.dom.input.addEventListener('blur', () => {
-            Utils.removeClass(this.element, '-focused');
+        return this;
+    }
 
-            Utils.ifNodeList(this.dom.labels, () => {
-                Utils.makeElementsFocusable(this.dom.labels);
-            });
-        });
 
-        this.dom.input.addEventListener('change', () => {
-            if (this.dom.input.value == '') {
-                Utils.removeClass(this.element, '-has-value');
-            } else {
-                Utils.addClass(this.element, '-has-value');
-            }
+    focusHandler() {
+        Utils.addClass(this.element, '-focused');
+
+        Utils.ifNodeList(this.dom.labels, () => {
+            Utils.makeElementsNotFocusable(this.dom.labels);
         });
 
         return this;
     }
+
+
+    blurHandler() {
+        Utils.removeClass(this.element, '-focused');
+
+        Utils.ifNodeList(this.dom.labels, () => {
+            Utils.makeElementsFocusable(this.dom.labels);
+        });
+
+        return this;
+    }
+
+
+    changeValueHandler() {
+        if (this.dom.input.value == '') {
+            Utils.removeClass(this.element,  '-has-value');
+
+            Utils.removeClass(this.element,  '-valid');
+            Utils.removeClass(this.element,  '-invalid');
+
+            Utils.removeClass(this.dom.hint, '-valid');
+            Utils.removeClass(this.dom.hint, '-invalid');
+
+            Utils.removeClass(this.dom.indicator, '-valid');
+            Utils.removeClass(this.dom.indicator, '-invalid');
+        } else {
+            Utils.addClass(this.element, '-has-value');
+
+            let validationTimeout = this.state.validationTimeout;
+
+            if (validationTimeout) {
+                clearTimeout(validationTimeout);
+            }
+
+            validationTimeout = setTimeout(this.validate.bind(this), this.state.validationDelay);
+        }
+
+        return this;
+    }
+
+    
+    validate() {
+        if (this.state.regexp.test(this.dom.input.value)) {
+            Utils.removeClass(this.element,       '-invalid');
+            Utils.addClass(this.element,          '-valid');
+
+            Utils.removeClass(this.dom.hint,      '-invalid');
+            Utils.addClass(this.dom.hint,         '-valid');
+
+            Utils.removeClass(this.dom.indicator, '-invalid');
+            Utils.addClass(this.dom.indicator,    '-valid');
+        } else {
+            Utils.removeClass(this.element,       '-valid');
+            Utils.addClass(this.element,          '-invalid');
+
+            Utils.removeClass(this.dom.hint,      '-valid');
+            Utils.addClass(this.dom.hint,         '-invalid');
+
+            Utils.removeClass(this.dom.indicator, '-valid');
+            Utils.addClass(this.dom.indicator,    '-invalid');
+        }
+
+        this.state.validationTimeout = null;
+
+        return this;
+    }    
 }
