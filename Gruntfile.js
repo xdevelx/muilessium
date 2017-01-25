da838ade2eafd87bf6440b903f2df50999223525
Ivan Bogachev <sfi0zy@gmail.com>
2017-01-25 18:00:14 +0300

    Add automatic favicons generation, Some minor changes in docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -240,6 +240,73 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+
+        realFavicon: {
+            favicons: {
+                src: 'src/docs_template/images/muilessium.png',
+                dest: 'docs/icons/',
+                options: {
+                    iconsPath: 'icons/',
+                    html: ['docs/*.html'],
+                    design: {
+                        ios: {
+                            pictureAspect: 'backgroundAndMargin',
+                            backgroundColor: '#ffffff',
+                            margin: '11%',
+                            assets: {
+                                ios6AndPriorIcons: false,
+                                ios7AndLaterIcons: false,
+                                precomposedIcons: false,
+                                declareOnlyDefaultIcon: true
+                            },
+                            appName: 'Muilessium'
+                        },
+                        desktopBrowser: {},
+                        windows: {
+                            pictureAspect: 'noChange',
+                            backgroundColor: '#383838',
+                            onConflict: 'override',
+                            assets: {
+                                windows80Ie10Tile: false,
+                                windows10Ie11EdgeTiles: {
+                                small: true,
+                                medium: false,
+                                big: false,
+                                rectangle: false
+                                }
+                            },
+                            appName: 'Muilessium'
+                        },
+                        androidChrome: {
+                            pictureAspect: 'shadow',
+                            themeColor: '#ffffff',
+                            manifest: {
+                                name: 'Muilessium',
+                                display: 'standalone',
+                                orientation: 'notSet',
+                                onConflict: 'override',
+                                declared: true
+                            },
+                            assets: {
+                                legacyIcon: false,
+                                lowResolutionIcons: false
+                            }
+                        },
+                        safariPinnedTab: {
+                            pictureAspect: 'blackAndWhite',
+                            threshold: 90,
+                            themeColor: '#383838'
+                        }
+                    },
+                    settings: {
+                        compression: 1,
+                        scalingAlgorithm: 'Lanczos',
+                        errorOnImageTooSmall: false
+                    }
+                }
+            }
+        },
         
         clean: {
             build: {
@@ -247,12 +314,31 @@ module.exports = function(grunt) {
                     'dist',
                     'docs'
                 ]
+            },
+            docs: {
+                src: [
+                    'docs/assets/less',
+                    'docs/*.jst',
+                    'docs/muilessium.css'
+                ]
             }
         }
     });
     
-    grunt.registerTask('default', ['lesslint', 'less', 'cssmin', 'browserify', 'uglify', 'dss', 'docco', 'copy']);
+    grunt.registerTask('default', [
+        'lesslint',
+        'less',
+        'cssmin',
+        'browserify',
+        'uglify',
+        'dss',
+        'docco',
+        'copy',
+        'realFavicon',
+        'clean:docs'
+    ]);
+
     grunt.registerTask('server',  ['default', 'browserSync', 'watch']);
-    grunt.registerTask('rebuild', ['clean', 'default']);
+    grunt.registerTask('rebuild', ['clean:build', 'default']);
     grunt.registerTask('test',    ['lesslint']);
 };
